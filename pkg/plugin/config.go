13647cfc2d1788937a9e27535e9811b0e846395c
Qinhang Li <qinhang@google.com>
2023-11-02 12:17:35 -0700

    feat: update config.go to have validate to ToFlags function (#8)
    * Feat: update config to have Validate and ToFlags function
    
    * tidy go.mod
    
    * fix nits

--- pkg/plugin/config.go
+++ pkg/plugin/config.go
@@ -15,13 +15,66 @@
 // Package plugin provides the implementation of the JVS plugin interface.
 package plugin
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/abcxyz/pkg/cli"
+)
+
 // PluginConfig defines the set over environment variables required
 // for running the plugin.
 type PluginConfig struct {
 	// ID of the GitHub APP we use to authenticate.
-	GitHubAppID string `env:"GITHUB_APP_ID,required"`
+	GitHubAppID string
 	// Installation ID of the github app.
-	GitHubAppInstallationID string `env:"GITHUB_APP_INSTALLATION_ID,required"`
+	GitHubAppInstallationID string
 	// The private Key PEM obtained for github app.
-	GitHubAppPrivateKeyPEM string `env:"GITHUB_APP_PRIVATE_KEY_PEM,required"`
+	GitHubAppPrivateKeyPEM string
+}
+
+// Validate validates if the config is valid.
+func (cfg *PluginConfig) Validate() error {
+	var rErr error
+	if cfg.GitHubAppID == "" {
+		rErr = errors.Join(rErr, fmt.Errorf("GITHUB_APP_ID is empty"))
+	}
+	if cfg.GitHubAppInstallationID == "" {
+		rErr = errors.Join(rErr, fmt.Errorf("GITHUB_APP_INSTALLATION_ID is empty"))
+	}
+	if cfg.GitHubAppPrivateKeyPEM == "" {
+		rErr = errors.Join(rErr, fmt.Errorf("$GITHUB_APP_PRIVATE_KEY_PEM is empty"))
+	}
+
+	return rErr
+}
+
+// ToFlags binds the config to the give [cli.FlagSet] and returns it.
+func (cfg *PluginConfig) ToFlags(set *cli.FlagSet) *cli.FlagSet {
+	// Command options
+	f := set.NewSection("GITHUB PLUGIN OPTIONS")
+
+	f.StringVar(&cli.StringVar{
+		Name:    "github-app-id",
+		Target:  &cfg.GitHubAppID,
+		EnvVar:  "GITHUB_APP_ID",
+		Example: "111111",
+		Usage:   "The ID of the github app.",
+	})
+
+	f.StringVar(&cli.StringVar{
+		Name:    "github-app-installation-id",
+		Target:  &cfg.GitHubAppInstallationID,
+		EnvVar:  "GITHUB_APP_INSTALLATION_ID",
+		Example: "project = JRA and assignee != jsmith",
+		Usage:   "The installation ID of the github app.",
+	})
+
+	f.StringVar(&cli.StringVar{
+		Name:   "github-app-private-key-pem",
+		Target: &cfg.GitHubAppPrivateKeyPEM,
+		EnvVar: "GITHUB_APP_PRIVATE_KEY_PEM",
+		Usage:  "The private key pem obtained for github app.",
+	})
+	return set
 }
